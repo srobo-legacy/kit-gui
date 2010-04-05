f382c9a5b23f4fdbd61eb2893706afb7612e628d
Sam Phippen <samphippen@googlemail.com>
2010-04-05 20:35:32 +0100

    Saner matchbox panel options
    Matchbox panel now appears at the top of the screen, and by default only the sr applet is apparent in it, as opposed to whatever the matchbox system defaults are

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ MYDISP=":3"
 Xephyr -screen 480x272 $MYDISP&
 sleep 3
 DISPLAY="$MYDISP"
-matchbox-panel&
+matchbox-panel -ns --orientation north&
 matchbox-window-manager -use_titlebar no&
 python sroboapplet.py
 
